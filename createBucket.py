e40c40b4eed72f1128c7be18755b60f1111c8720
Ravi Kushwah <62547898+Ravi0021@users.noreply.github.com>
2023-07-03 17:32:34 +0530

    Update createBucket.py

--- createBucket.py
+++ createBucket.py
@@ -1,26 +1,47 @@
+import sys
 import boto3
-
+from datetime import datetime, timezone
 s3 = boto3.client('s3')
+latest_versions = {}
 
-# Set the bucket name and prefix (optional)
-bucket_name = 'ravi-testing-bucket'
-prefix = 'input-data-files'
-
-# Set the page size and start token (optional)
-page_size = 1000
-start_token = None
-
-# Iterate over all object versions using pagination
-while True:
-    # Call the list_object_versions() method to get a page of object versions
-    response = s3.list_object_versions(Bucket=bucket_name, Prefix=prefix, MaxKeys=page_size)
-    
-    # Process the object versions in the response
+# Extract all latest version of each file
+def extractLatestVersions(response,modified_date):
     for version in response['Versions']:
-        print(version['Key'])
+        last_modified = version['LastModified']
+        if last_modified < modified_date:
+            key = version['Key']
+            if key not in latest_versions or latest_versions[key]['LastModified'] < version['LastModified']:
+                latest_versions[key] = version
+
+# Copy all latest version of each file to another location
+def copyLatestFiles(bucket_name, output_prefix):
+    for version in latest_versions.values():
+        file_path = version['Key']
+        path_parts = file_path.split('/')
+        s3.copy_object(
+        Bucket=bucket_name,
+        CopySource={
+            'Bucket': bucket_name,
+            'Key': version['Key'],
+            'VersionId': version['VersionId']
+        },
+        Key=output_prefix+'/'+path_parts[-1]
+        )
+        # print(f"Object key: {version['Key']}, version id: {version['VersionId']}, last modified: {version['LastModified']}")
+
+# main method
+def main(bucket_name, input_prefix, output_prefix, modified_date):
+    paginator = s3.get_paginator('list_object_versions')
+    page_iterator = paginator.paginate(Bucket=bucket_name, Prefix=input_prefix)
+    for page in page_iterator:
+        extractLatestVersions(page, modified_date)
+    copyLatestFiles(bucket_name,output_prefix)
     
-    # Check if there are more pages of object versions to retrieve
-    if response['IsTruncated']:
-        start_token = response['NextKeyMarker']
-    else:
-        break
+
+if __name__ == "__main__":
+    bucket = sys.argv[1]
+    inputPathPrefix = sys.argv[2]
+    outputPathPrefix = sys.argv[3]
+    date = sys.argv[4]
+    modifiedDate=datetime.fromisoformat(date).replace(tzinfo=timezone.utc)
+    main(bucket,inputPathPrefix,outputPathPrefix,modifiedDate)
