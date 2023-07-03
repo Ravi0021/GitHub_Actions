5adb472587b1276a77c4ae64b3f1665ac21c3eea
Ravi Kushwah <62547898+Ravi0021@users.noreply.github.com>
2023-07-03 17:48:41 +0530

    Update createBucket.py

--- createBucket.py
+++ createBucket.py
@@ -10,7 +10,8 @@ def extract_latest_versions(response,modified_date):
         last_modified = version['LastModified']
         if last_modified < modified_date:
             key = version['Key']
-            if key not in latest_versions or latest_versions[key]['LastModified'] < version['LastModified']:
+            if key not in latest_versions or latest_versions[key]['LastModified'] \
+            < version['LastModified']:
                 latest_versions[key] = version
 
 # Copy all latest version of each file to another location
@@ -27,21 +28,19 @@ def copy_latest_files(bucket_name, output_prefix):
         },
         Key=output_prefix+'/'+path_parts[-1]
         )
-        # print(f"Object key: {version['Key']}, version id: {version['VersionId']}, last modified: {version['LastModified']}")
 
 # main method
 def main(bucket_name, input_prefix, output_prefix, modified_date):
     paginator = s3.get_paginator('list_object_versions')
-    page_iterator = paginator.paginate(Bucket=bucket_name, Prefix=input_prefix)
+    page_iterator = paginator.paginate(Bucket = bucket_name, Prefix = input_prefix)
     for page in page_iterator:
-        extractLatestVersions(page, modified_date)
-    copyLatestFiles(bucket_name,output_prefix)
-    
+        extract_latest_versions(page, modified_date)
+    copy_latest_files(bucket_name,output_prefix)
 
 if __name__ == "__main__":
-    bucket = sys.argv[1]
+    BUCKET = sys.argv[1]
     inputPathPrefix = sys.argv[2]
     outputPathPrefix = sys.argv[3]
     date = sys.argv[4]
     modifiedDate=datetime.fromisoformat(date).replace(tzinfo=timezone.utc)
-    main(bucket,inputPathPrefix,outputPathPrefix,modifiedDate)
+    main(BUCKET,inputPathPrefix,outputPathPrefix,modifiedDate)
